aee2c08d70c526c474d573f9393a0e4160f18c9a
r <deliberatethink@gmail.com>
2019-12-13 15:53:27 +0530

    Update callbacks0.js

--- callbacks0.js
+++ callbacks0.js
@@ -1,3 +1,118 @@
-function multiplyByTwo(num) {
-  return 2 * num; 
+// Type JavaScript here and click "Run Code" or press Ctrl + s
+console.log('Hello, world!');
+
+
+// Challenge 1
+function addTwo(num) {
+	return num + 2;
 }
+
+// To check if you've completed it, uncomment these console.logs!
+// console.log(addTwo(3));
+// console.log(addTwo(10));
+
+
+// Challenge 2
+function addS(word) {
+	return word + "s";
+}
+
+// uncomment these to check your work
+// console.log(addS('pizza'));
+// console.log(addS('bagel'));
+
+
+// Challenge 3
+function map(array, callback) {
+	const result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(callback(array[i]));
+  }
+  return result;
+}
+
+ console.log(map([1, 2, 3], addTwo));
+
+
+// Challenge 4
+function forEach(array, callback) {
+	for (let i = 0; i < array.length; i++) {
+    callback(array[i]);
+  }
+}
+
+// see for yourself if your forEach works!
+forEach([1,2,3], ele=>console.log(ele));
+
+//--------------------------------------------------
+// Extension
+//--------------------------------------------------
+
+//Extension 1
+function mapWith(array, callback) {
+  const result = [];
+	forEach(array, ele=>result.push(ele));
+  return result;
+}
+
+console.log(mapWith([1, 2, 3], addTwo));
+
+
+
+//Extension 2
+function reduce(array, callback, initialValue) {
+	var accumulatedValue = initialValue;
+  for (let i = 0; i < array.length; i++) {
+    accumulatedValue = callback(accumulatedValue, array[i]);
+  }
+  return accumulatedValue;
+  
+}
+console.log(reduce([4, 1, 3], (a, b) => a + b, 0));
+
+//Extension 3
+function intersection(arrays) {
+
+}
+
+// console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
+// should log: [5, 15]
+
+//Extension 4
+function union(arrays) {
+
+}
+
+// console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
+// should log: [5, 10, 15, 88, 1, 7, 100]
+
+//Extension 5
+function objOfMatches(array1, array2, callback) {
+
+}
+
+// console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], function(str) { return str.toUpperCase(); }));
+// should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
+
+//Extension 6
+function multiMap(arrVals, arrCallbacks) {
+
+}
+
+// console.log(multiMap(['catfood', 'glue', 'beer'], [function(str) { return str.toUpperCase(); }, function(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }, function(str) { return str + str; }]));
+// should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
+
+
+//Extension 7
+function objectFilter(obj, callback) {
+
+}
+
+// const cities = {
+// London: 'LONDON',
+// LA: 'Los Angeles',
+// Paris: 'PARIS',
+// };
+// console.log(objectFilter(cities, city => city.toUpperCase())) // Should log { London: 'LONDON', Paris: 'PARIS'}
+
+
