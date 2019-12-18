385f86cbb41d02c8b60647ae86d5138bae7fa029
rj <rraj411@gmail.com>
2019-12-18 22:29:22 +0530

    closure and callback done

--- callbacks0.js
+++ callbacks0.js
@@ -72,47 +72,95 @@ console.log(reduce([4, 1, 3], (a, b) => a + b, 0));
 
 //Extension 3
 function intersection(arrays) {
-
+	return reduce(arrays, function intersect(arrayA, arrayB) {
+    const result = [];
+    for (let i = 0; i < arrayA.length; i++) {
+      for (let j = 0; j < arrayB.length; j++) {
+        if (arrayA[i] == arrayB[j]) {
+          result.push(arrayA[i]);
+          break;
+        }
+      }
+    }
+    return result;
+  }, arrays[0]);
 }
 
-// console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
+console.log(intersection([[5, 10, 15, 20], [15, 5, 30, 20], [1, 2, 3, 4, 5, 15, 8]]));
 // should log: [5, 15]
 
 //Extension 4
 function union(arrays) {
-
+  return reduce(arrays, function unionTwo(arrayA, arrayB) {
+    const result = [];
+    const arrA = [...arrayA]; // assume remove duplicates also
+    const arrB = [...arrayB];
+    for (let i = 0; i < arrA.length; i++) {
+      result.push(arrA[i]);
+      for (let j = 0; j < arrB.length; j++) {
+        if (arrA[i] == arrB[j]) {
+          arrB[j] = undefined;
+        }
+      }
+    }
+    for (let i = 0; i < arrB.length; i++) {
+      if (arrB[i] != undefined) {
+        result.push(arrB[i]);
+      }
+    }
+    return result;
+  }, arrays[0])
 }
 
-// console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
+console.log(union([[5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]]));
 // should log: [5, 10, 15, 88, 1, 7, 100]
 
 //Extension 5
 function objOfMatches(array1, array2, callback) {
-
+  const result = {};
+  for (let i = 0; i < array1.length; i++) {
+    if ( callback(array1[i]) == array2[i] ) {
+      result[array1[i]] = array2[i];
+    }
+  }
+  return result;
 }
 
-// console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], function(str) { return str.toUpperCase(); }));
+console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], function(str) { return str.toUpperCase(); }));
 // should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
 
 //Extension 6
 function multiMap(arrVals, arrCallbacks) {
-
+  const result = {};
+  for (let val of arrVals) {
+    result[val] = [];
+    for (let fn of arrCallbacks) {
+      result[val].push(fn(val));
+    }
+  }
+  return result;
 }
 
-// console.log(multiMap(['catfood', 'glue', 'beer'], [function(str) { return str.toUpperCase(); }, function(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }, function(str) { return str + str; }]));
+console.log(multiMap(['catfood', 'glue', 'beer'], [function(str) { return str.toUpperCase(); }, function(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }, function(str) { return str + str; }]));
 // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
 
 
 //Extension 7
 function objectFilter(obj, callback) {
-
+  const result = {};
+  for (var key in obj) {
+    if (callback(key) == obj[key])  {
+      result[key] = key;
+    }
+  }
+  return result;
 }
 
-// const cities = {
-// London: 'LONDON',
-// LA: 'Los Angeles',
-// Paris: 'PARIS',
-// };
-// console.log(objectFilter(cities, city => city.toUpperCase())) // Should log { London: 'LONDON', Paris: 'PARIS'}
-
+const cities = {
+London: 'LONDON',
+LA: 'Los Angeles',
+Paris: 'PARIS',
+};
+//
+console.log(objectFilter(cities, city => city.toUpperCase())) // Should log { London: 'LONDON', Paris: 'PARIS'}
 
