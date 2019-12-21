00b936f600434d19cc8b3797d522284279c2b169
r <deliberatethink@gmail.com>
2019-12-21 22:13:10 +0530

    Create oop0.js

---
+++ oop0.js
@@ -0,0 +1,2 @@
+arrow functions -> this is bound to where it is stored not where it is run
+you cannot do oop like stuff with arrow functions because arrow functions dont use the rule that fill this with user1.connect()
